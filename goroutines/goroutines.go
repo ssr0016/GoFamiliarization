afca006fa473d8dbe77c23c18c134e08f39a8a42
ssr0016 <samsonrecluta0016@gmail.com>
2023-10-28 10:08:04 +0800

    study goroutines

---
+++ goroutines/goroutines.go
@@ -0,0 +1,38 @@
+package main
+
+//A goroutine is a lightweight thread of execution.
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
+
+func main() {
+
+	f("direct") //Suppose we have a function call f(s). Here’s how we’d call that in the usual way, running it synchronously.
+
+	go f("goroutine") //To invoke this function in a goroutine, use go f(s). This new goroutine will execute concurrently with the calling one.
+
+	go func(msg string) { //You can also start a goroutine for an anonymous function call.
+		fmt.Println(msg)
+	}("going")
+
+	time.Sleep(time.Second) //Our two function calls are running asynchronously in separate goroutines now. Wait for them to finish (for a more robust approach, use a WaitGroup).
+	fmt.Println("done")
+}
+
+// $ go run goroutines.go // When we run this program, we see the output of the blocking call first, then the output of the two goroutines. The goroutines’ output may be interleaved, because goroutines are being run concurrently by the Go runtime.
+// direct : 0
+// direct : 1
+// direct : 2
+// goroutine : 0
+// going
+// goroutine : 1
+// goroutine : 2
+// done
